b277acfed1301ab1102aaa673c3551c83e55f78d
mehdi <najafian@live.com>
2022-07-22 21:37:20 +0200

    first commit

---
+++ src/test/kotlin/com/artingame/logger/LoggerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.artingame.logger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoggerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
